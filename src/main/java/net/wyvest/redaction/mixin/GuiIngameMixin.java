e959e8b1709703095dde91ed66aa6a161f573051
Wyvest <45589059+Wyvest@users.noreply.github.com>
2021-08-25 00:50:00 +0900

    Okay

--- src/main/java/net/wyvest/redaction/mixin/GuiIngameMixin.java
+++ src/main/java/net/wyvest/redaction/mixin/GuiIngameMixin.java
@@ -1,23 +1,40 @@
 package net.wyvest.redaction.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.wyvest.redaction.blackbar.BlackBarManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiIngame.class)
-public class GuiIngameMixin {
+public abstract class GuiIngameMixin {
 
-    @Redirect(method = "renderTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V"))
-    private void doNothing(TextureManager textureManager, ResourceLocation resource) {
+    @Shadow protected abstract void renderHotbarItem(int index, int xPos, int yPos, float partialTicks, EntityPlayer player);
 
-    }
-
-    @Redirect(method = "renderTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiIngame;drawTexturedModalRect(IIIIII)V"))
-    private void doNothingAgain(GuiIngame guiIngame, int x, int y, int textureX, int textureY, int width, int height) {
-
+    @Inject(method = "renderTooltip", at = @At("HEAD"), cancellable = true)
+    private void cancel(ScaledResolution res, float partialTicks, CallbackInfo ci) {
+        ci.cancel();
+        BlackBarManager.INSTANCE.render(res, partialTicks);
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+        net.minecraft.client.renderer.RenderHelper.enableGUIStandardItemLighting();
+        for (int j = 0; j < 9; ++j)
+        {
+            int k = res.getScaledWidth() / 2 - 90 + j * 20 + 2;
+            int l = res.getScaledHeight() - 16 - 3;
+            renderHotbarItem(j, k, l, partialTicks, (EntityPlayer) Minecraft.getMinecraft().getRenderViewEntity());
+        }
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.disableBlend();
     }
 
 }
--- src/main/kotlin/net/wyvest/redaction/blackbar/BlackBarManager.kt
+++ src/main/kotlin/net/wyvest/redaction/blackbar/BlackBarManager.kt
@@ -3,10 +3,12 @@ package net.wyvest.redaction.blackbar
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiChat
 import net.minecraft.client.gui.ScaledResolution
-import net.minecraftforge.client.event.RenderGameOverlayEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import net.wyvest.redaction.blackbar.text.CpsText
-import net.wyvest.redaction.blackbar.text.TestText
+import net.minecraft.client.renderer.GlStateManager
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.ResourceLocation
+import net.wyvest.redaction.Redaction.mc
+import xyz.matthewtgm.requisite.data.ColourRGB
+import xyz.matthewtgm.requisite.util.GlHelper
 import xyz.matthewtgm.requisite.util.MathHelper
 import xyz.matthewtgm.requisite.util.RenderHelper
 import java.awt.Color
@@ -16,36 +18,52 @@ object BlackBarManager {
     private var data: BlackBarData? = null
 
     private val color = Color(0, 0, 0, 85)
-    var text = listOf(
-        CpsText(),
-        TestText()
-    )
-
+    private val white = ColourRGB(Color.WHITE)
+    private val texPath = ResourceLocation("textures/gui/widgets.png")
     private var firstTime = true
 
     fun initialize() {
         val resolution = ScaledResolution(Minecraft.getMinecraft())
-        data = BlackBarData(resolution.scaledHeight - 22)
+        data = BlackBarData(-1.0F, resolution.scaledHeight - 22)
     }
 
-    @SubscribeEvent
-    fun render(event: RenderGameOverlayEvent.Pre) {
+    fun render(
+        res: ScaledResolution,
+        partialTicks: Float
+    ) {
         data?.let {
-            if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR) {
-                val scaledWidth = event.resolution.scaledWidth
-                val scaledHeight = event.resolution.scaledHeight
+            if (mc.renderViewEntity is EntityPlayer) {
+                val entityplayer = mc.renderViewEntity as EntityPlayer
+                GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f)
+                mc.textureManager.bindTexture(texPath)
+                val scaledWidth = res.scaledWidth
+                val scaledHeight = res.scaledHeight
                 it.hiding = Minecraft.getMinecraft().currentScreen is GuiChat
                 if (firstTime) {
                     firstTime = false
-                    it.y = event.resolution.scaledHeight - 22
+                    it.x = scaledWidth / 2 - 90.5F + entityplayer.inventory.currentItem * 20
+                    it.y = scaledHeight - 22
                     it.hiding = false
                 }
                 if (it.hiding) {
-                    it.y = MathHelper.lerp(it.y.toFloat(), scaledHeight.toFloat() + 2, event.partialTicks / 4).toInt()
+                    it.y = MathHelper.lerp(it.y.toFloat(), scaledHeight.toFloat() + 2, partialTicks / 4).toInt()
                 } else if (!it.hiding && it.y != scaledHeight - 22) {
-                    it.y = MathHelper.lerp(it.y.toFloat(), scaledHeight.toFloat() - 22, event.partialTicks / 4).toInt()
+                    it.y = MathHelper.lerp(it.y.toFloat(), scaledHeight.toFloat() - 22, partialTicks / 4).toInt()
+                }
+                if (it.lastSlot != entityplayer.inventory.currentItem) {
+                    if (scaledWidth / 2 - 90.5F + entityplayer.inventory.currentItem * 20 != it.x) {
+                        it.x = MathHelper.lerp(it.x, scaledWidth / 2 - 90.5F + entityplayer.inventory.currentItem * 20, partialTicks / 4)
+                    } else {
+                        it.lastSlot = entityplayer.inventory.currentItem
+                    }
+                } else {
+                    it.lastSlot = entityplayer.inventory.currentItem
+                    it.x = scaledWidth / 2 - 90.5F + entityplayer.inventory.currentItem * 20
                 }
                 RenderHelper.drawRectEnhanced(0, it.y, scaledWidth, 22, color.rgb)
+                GlHelper.drawRectangle(it.x,
+                    it.y.toFloat(), 22F, 22F, white
+                )
             }
         }
     }
@@ -53,7 +71,9 @@ object BlackBarManager {
 }
 
 private class BlackBarData(
+    var x: Float,
     var y: Int
 ) {
     var hiding = false
+    var lastSlot = 4
 }
