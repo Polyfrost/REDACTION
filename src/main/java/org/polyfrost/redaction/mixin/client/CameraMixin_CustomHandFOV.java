2e364504144423cbda68f465479a678c51454634
MicrocontrollersDev <microcontrollers.dev@pm.me>
2026-07-31 23:09:44 -0700

    chore: replace modifyconstant

--- src/main/java/org/polyfrost/redaction/mixin/client/CameraMixin_CustomHandFOV.java
+++ src/main/java/org/polyfrost/redaction/mixin/client/CameraMixin_CustomHandFOV.java
@@ -1,16 +1,16 @@
 package org.polyfrost.redaction.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.polyfrost.redaction.client.RedactionConfig;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 //~ if <26.1 'Camera' -> 'renderer.GameRenderer'
 @Mixin(net.minecraft.client.Camera.class)
 abstract class CameraMixin_CustomHandFOV {
     //~ if <26.1 'calculateHudFov' -> 'getFov'
     //~ if 1.21.1 'float' -> 'double' {
-    @ModifyConstant(method = "calculateHudFov", constant = @Constant(floatValue = 70))
+    @ModifyExpressionValue(method = "calculateHudFov", at = @At(value = "CONSTANT", args = "floatValue=70"))
     private float modifyFov(float original) {
         if (RedactionConfig.INSTANCE.getCustomHandFOV()) {
             return RedactionConfig.INSTANCE.getHandFOV();
